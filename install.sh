20e3b095939741fb434398935055c15740e8e077
zabojeb <zabojeb@bk.ru>
2024-11-27 00:13:44 +0300

    v1.4: Total fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-fetch https://raw.githubusercontent.com/zabojeb/phgpg/refs/heads/main/phgpg.py
+curl https://raw.githubusercontent.com/zabojeb/phgpg/refs/heads/main/phgpg.py >phgpg.py
 sudo chmod +x phgpg.py
-sudo mv phgpp.py /usr/local/bin/phgpg
+sudo mv phgpg.py /usr/local/bin/phgpg
 
 echo "phgpg succesfully installed!"
